78bf65bd5ed6059d41ed8f890dd6517ced146db2
Theodor S. Adamsen <theodorsebastian@hotmail.com>
2019-11-21 08:56:18 +0100

    change to test

--- src/test/java/dk/eaaa/BowlingFrameTest.java
+++ src/test/java/dk/eaaa/BowlingFrameTest.java
@@ -36,7 +36,7 @@ public class BowlingFrameTest {
 	
 	@Test
 	public void testIsSpare() throws Exception {
-		frame.rollFirst(5);
+		frame.rollFirst(2);
 		frame.rollSecond(5);
 		assertTrue(frame.isSpare());
 	}
